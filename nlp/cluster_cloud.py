752ba93008fdedd133c6036fe9d420099a171d8b
sam osebe <ms.osebe@gmail.com>
2022-02-28 20:06:41 -0500

    sentence cluster

--- nlp/cluster_cloud.py
+++ nlp/cluster_cloud.py
@@ -8,16 +8,22 @@ import requests
 model = SentenceTransformer("sentence-transformers/paraphrase-mpnet-base-v2")
 clusters = np.load("reduced_dime.npy")
 
-
-# rectangle = [-59, -39, 30, 10]  # cluster 1
-# rectangle = [-39, -18, 40, 20]  # cluster 2
-# rectangle = [-20, 1, 60, 40]  # cluster 3
-# rectangle = [31, 42, 20, 4]  # cluster 4
-# rectangle = [48, 60, 12, -2]  # cluster 5
-# rectangle = [48, 60, -2, -20]  # cluster 6
-# rectangle = [0, 20, 10, -10]  # center of sphere
-rectangle = [-45, -20, -10, -28]  # cluster 7
-cluster_name = "../plots/word_cloud_cluster_7.png"
+clusters_dims = {
+    0: [0, 20, 10, -10],      # center of sphere
+    1: [-59, -39, 30, 10],    # cluster 1
+    2: [-39, -18, 40, 20],    # cluster 2
+    3: [-20, 1, 60, 40],      # cluster 3
+    4: [31, 42, 20, 4],       # cluster 4
+    5: [48, 60, 12, -2],      # cluster 5
+    6: [48, 60, -2, -20],     # cluster 6
+    7: [-45, -20, -10, -28],  # cluster 7
+    8: [2, 30, -33, -60],     # cluster 8
+    9: [-18, 2, -20, -40],    # bottom left
+    10: [-30, -10, 10, -18]   # left
+}
+cluster = 10
+rectangle = clusters_dims[cluster]
+cluster_name = "../plots/word_cloud_cluster_"+str(cluster)+".png"
 
 # Sentences
 sentences = np.load("sentences.npy")
@@ -34,7 +40,8 @@ for i in range(len(sample_sentences)):
 
 # cloud
 pic = np.array(Image.open(requests.get(
-    'http://www.clker.com/cliparts/2/9/b/8/1194984775760075334button-green_benji_park_01.svg.med.png',stream=True).raw))
+    'http://www.clker.com/cliparts/2/9/b/8/1194984775760075334button-green_benji_park_01.svg.med.png',
+    stream=True).raw))
 
 # with collocations
 wordcloud = WordCloud(width=1000, height=1000, background_color='white', mask=pic,
