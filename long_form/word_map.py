eecd0718d8ec3f5c2a97c8086e39afce558ac007
sam osebe <ms.osebe@gmail.com>
2022-09-26 13:56:17 -0400

    word map

--- long_form/word_map.py
+++ long_form/word_map.py
@@ -1,30 +1,37 @@
 import json
 from transformers import AutoTokenizer
+from datasets import load_dataset
 
 tokenizer = AutoTokenizer.from_pretrained("roberta-large")
+wikitext = load_dataset("wikitext", "wikitext-2-v1")
 
-word_file = "/Users/samosebe/Downloads/wordNetJson/wn.json"
+word_file = "../data/wordNetJson/wn.json"
 with open(word_file, "r") as file:
     word_net = json.loads(file.read())
-
-words = word_net["words"]
-synonyms = word_net["synsets"]
-
 word_map = {}
-for word in words:
-    key = words[word][0]
-    if "synonyms" in synonyms[key]:
-        syn = synonyms[key]["synonyms"]
 
-        w = key
-        w_token = max(tokenizer.encode(w))
-        s = syn[0]
-        s_token = max(tokenizer.encode(s))
-        if w_token != s_token:
-            word_map[w_token] = s_token
-            word_map[s_token] = w_token
-        print(w, w_token, s, s_token)
 
-with open("token_map", "w") as file:
+def process(wiki):
+    for i in range(len(wiki["text"])):
+        text = wiki["text"][i]
+        words = text.split(" ")
+        if len(words) > 20:
+            for word in words:
+                word = str.lower(word.strip())
+                if word in word_net["words"] and word not in word_map:
+                    keys = word_net["words"][word]
+                    if len(keys) > 0:
+                        related_words = word_net["synsets"][keys[0]]
+                        if "synonyms" in related_words:
+                            synonymns = related_words["synonyms"]
+                            synonym = str.lower(synonymns[0].strip())
+                            if word != synonym:
+                                word_map[word] = synonym
+                                word_map[synonym] = word
+
+
+wikitext["train"].map(process, batched=True, remove_columns=wikitext["train"].column_names)
+
+with open("token_map.json", "w") as file:
     file.write(json.dumps(word_map))
 
